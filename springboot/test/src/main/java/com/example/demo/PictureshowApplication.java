42f760516ffc5784b8978f7008b8027455bbb575
githublms <37056964+githublms@users.noreply.github.com>
2019-03-27 10:37:02 +0800

    任意的测试内容

---
+++ springboot/test/src/main/java/com/example/demo/PictureshowApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.io.File;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import cn.hutool.core.io.FileUtil;
+
+@SpringBootApplication
+public class PictureshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureshowApplication.class, args);
+	}
+
+	
+
+}
+
